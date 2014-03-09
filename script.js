f279d75e4b3bf7d87a5906fb15fc1beb95bcc0c6
Greg Dziemidowicz <greg@cogent.co>
2014-03-09 15:41:15 +1100

    Something is working

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
 var myAppModule = angular.module('app', ['ui.date'])
 
 myAppModule.controller('TimeCtrl', function($scope) {
-
+  $scope.model = {
+    "date": "16 August, 1986"
+  }
   $scope.todoText = "Angular works";
+  
+  $scope.difference = function() {
+    
+    var pickedDate =  moment($scope.model.date);
+    
+    var now = new Date();
+    var diff = moment.preciseDiff(now.toLocaleDateString()  ,pickedDate)
+    return diff;
+  }
 
-  $scope.difference = "testing";
+  $scope.till = function(value) {
+    
+    var pickedDate =  moment($scope.model.date);
+    var forty = pickedDate.add('years', value);
+    console.log(forty);
+    var now = new Date();
+
+    var diff = moment.preciseDiff(now.toLocaleDateString(), forty)
+    return diff;
+  }
+
 
 });
