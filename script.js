582e0a6f4bd1c9997fdaff169fa84d0b0db3dc45
Greg Dziemidowicz <greg@cogent.co>
2014-03-09 17:08:46 +1100

    Supprot for query params and moreOrLessInWeeks added

--- script.js
+++ script.js
@@ -10,12 +10,16 @@ $(function(){
 
 var myAppModule = angular.module('app', [])
 
-myAppModule.controller('TimeCtrl', function($scope) {
-  $scope.model = {
-    "date": "08-16-1986"
-  }
-  $scope.todoText = "Angular works";
+myAppModule.controller('TimeCtrl', function($scope, $location) {
+
   
+  $scope.model = {
+    "date": ( $location.search().dob  ) || "08-16-1986"
+  }
+
+  $scope.name = $location.search().name
+  
+
   $scope.difference = function() {
     
     var pickedDate =  moment($scope.model.date);
@@ -25,15 +29,50 @@ myAppModule.controller('TimeCtrl', function($scope) {
     return diff;
   }
 
+  var diffrenceInGeneral = function() {
+    var pickedDate =  moment($scope.model.date);
+    
+    var now = moment(new Date().toLocaleDateString());
+    return moment.duration(now.diff(pickedDate));
+  }
+
+  $scope.differenceInDays = function() {
+    return Math.round(diffrenceInGeneral().asDays());
+  }
+
+
+  $scope.differenceInWeeks = function() {
+   return Math.round(diffrenceInGeneral().asWeeks());
+  }
+
   $scope.till = function(value) {
     
     var pickedDate =  moment($scope.model.date);
-    var forty = pickedDate.add('years', value);
+    var otherDate = pickedDate.add('years', value);
     var now = new Date();
 
-    var diff = moment.preciseDiff(now.toLocaleDateString(), forty)
+    var diff = moment.preciseDiff(now.toLocaleDateString(), otherDate)
     return diff;
   }
+
+  $scope.moreOrLess = function(value) {
+    
+    var pickedDate =  moment($scope.model.date);
+    var otherDate = pickedDate.add('years', value);
+    return otherDate.fromNow();
+  }
+
+  $scope.moreOrLessInWeeks = function(value) {
+    
+    var pickedDate =  moment($scope.model.date);
+    var otherDate = pickedDate.add('years', value);
+
+    var now = moment(new Date().toLocaleDateString());
+
+    return Math.round(moment.duration(otherDate.diff(now)).asWeeks());
+  }
+
+
 
 
 });
